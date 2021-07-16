b043f0e3c53059527a815cec38695b27abd303e1
iProov <support@iproov.com>
2021-07-16 16:59:44 +0000

    Release SDK 7.0.0

---
+++ example-app/app/src/main/java/com.iproov.example/Constants.kt
@@ -0,0 +1,8 @@
+// Copyright (c) 2020 iProov Ltd. All rights reserved.
+package com.iproov.example
+
+object Constants {
+    const val BASE_URL = "https://eu.rp.secure.iproov.me/api/v2/"
+    const val API_KEY = "" // TODO: Add your API key here
+    const val SECRET = "" // TODO: Add your Secret here
+}
--- waterloo-bank/app/src/main/java/uk/co/waterloobank/Constants.kt
+++
@@ -1,7 +0,0 @@
-package uk.co.waterloobank
-
-object Constants {
-    const val BASE_URL = "https://eu.rp.secure.iproov.me/api/v2/"
-    const val API_KEY = "" // TODO: Add your API key here
-    const val SECRET = "" // TODO: Add your Secret here
-}
